1487f39044de63f334aa506a44aea2978a14ebb3
hvaron83 <h.varon@uniandes.edu.co>
2021-04-27 03:27:24 +0000

    Linea base

---
+++ scripts/main.js
@@ -0,0 +1,3 @@
+import { Aprendiz, NivelEducativo } from "./aprendiz.js";
+export var ap = new Aprendiz("Hector Fabio", "Varon Bonilla", "avatar.png", 30, NivelEducativo.POSGRADO);
+console.log(ap);
